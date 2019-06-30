d548e216b991ad002ba898aaf24df234858ca7fe
Albert I <kras@raphielgang.org>
2019-06-30 22:39:03 +0800

    Convert to list of Cloudflare IPs
    Signed-off-by: Albert I <kras@raphielgang.org>

--- nginx-cf.php
+++ nginx-cf.php
@@ -3,6 +3,9 @@
 ?>
 # Generated with nginx-cf by @theel0ja
 # https://theel0ja.github.io/nginx-cf
+
+geo $realip_remote_addr $cloudflare_ip {
+    default		0;
 <?php
     $ipv4 = file_get_contents("https://www.cloudflare.com/ips-v4");
     $ipv6 = file_get_contents("https://www.cloudflare.com/ips-v6");
@@ -12,15 +15,17 @@
 
     foreach($ipv4 as $i) {
         if($i != "") {
-            echo "set_real_ip_from ". $i . ";\n";
+            echo "    ". $i . "	1;\n";
         }
     }
 
     foreach($ipv6 as $i) {
         if($i != "") {
-            echo "set_real_ip_from ". $i . ";\n";
+            echo "    ". $i . "	1;\n";
         }
     }
     
 ?>
+}
+
 real_ip_header CF-Connecting-IP;
