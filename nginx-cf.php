2924d00a69e601006a2783d3fdf9eab48d80d95e
Albert I <kras@raphielgang.org>
2019-07-01 00:50:32 +0800

    Restore actual behavior
    Signed-off-by: Albert I <kras@raphielgang.org>

--- nginx-cf.php
+++ nginx-cf.php
@@ -4,15 +4,17 @@
 # Generated with nginx-cf by @theel0ja
 # https://theel0ja.github.io/nginx-cf
 
-geo $realip_remote_addr $cloudflare_ip {
-    default		0;
 <?php
     $ipv4 = file_get_contents("https://www.cloudflare.com/ips-v4");
     $ipv6 = file_get_contents("https://www.cloudflare.com/ips-v6");
-    
+
     $ipv4 = explode("\n", $ipv4);
     $ipv6 = explode("\n", $ipv6);
+?>
 
+geo $realip_remote_addr $cloudflare_ip {
+    default		0;
+<?php
     foreach($ipv4 as $i) {
         if($i != "") {
             echo "    ". $i . "	1;\n";
@@ -24,8 +26,21 @@ geo $realip_remote_addr $cloudflare_ip {
             echo "    ". $i . "	1;\n";
         }
     }
-    
 ?>
 }
+
+<?php
+    foreach($ipv4 as $i) {
+        if($i != "") {
+            echo "set_real_ip_from ". $i . ";\n";
+        }
+    }
+
+    foreach($ipv6 as $i) {
+        if($i != "") {
+            echo "set_real_ip_from ". $i . ";\n";
+        }
+    }
+?>
 
 real_ip_header CF-Connecting-IP;
